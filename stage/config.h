cc90b316576e1f273171648661c10c3b93a0deb0
mylxiaoyi <mylxiaoyi@gmail.com>
2015-03-01 22:45:13 +0800

    make python_qt_binding to support qt5

--- stage/config.h
+++ stage/config.h
@@ -4,8 +4,8 @@
 #define PROJECT "Stage"
 #define VERSION "4.1.1"
 #define APIVERSION "4.1"
-#define INSTALL_PREFIX "/home/mylxiaoyi/source/ros_catkin_ws/install_isolated"
-#define PLUGIN_PATH "/home/mylxiaoyi/source/ros_catkin_ws/install_isolated/lib/Stage-4.1"
+#define INSTALL_PREFIX "/home/mylxiaoyi/source/ros_catkin_ws.py3/install_isolated"
+#define PLUGIN_PATH "/home/mylxiaoyi/source/ros_catkin_ws.py3/install_isolated/lib/Stage-4.1"
 
 /* #undef BUILD_GUI */
 
